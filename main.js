425fb6e69380272aa548fc18671e79eaf8634699
Christensen-Colter <51727656+Christensen-Colter@users.noreply.github.com>
2021-04-21 15:12:51 -0600

    Dynamic Links

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const links = [
-    "Week1 notes",
-    "week1/index.html",
-    "Week2 notes",
-    "week2/index.html"
-]
-
-function createLink(value) {
-  var x = document.createElement("li")
-  var t = document.createTextNode(value);
-  x.appendChild(t);
-  document.getElementById("demo").apendChild(x);
-}
+  { label: "Week 1", url: "index.html"}
+];
 
 function dynamicLinks() {
   for (i = 0; i < links.length; i++) {
-    //document.getElementById("demo").innerHTML = links[i];
-    document.createElement("li") = links[i];
-    //var t = document.createTextNode(links[i]);
-    //x.appendChild(t);
-    //document.getElementById("demo").apendChild(x);
-    //createLink(links[i]);
+        
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+
+    var text = document.createTextNode(links[i].label);
+    
+    a.appendChild(text);
+
+    a.title = links[i].label;
+    a.href = links[i].url;
+    
+    li.appendChild(a);
+    document.getElementById("oList").appendChild(li);
+  
   }
 
  
