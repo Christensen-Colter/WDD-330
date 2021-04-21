d803858851c35dc99882b40544a6b69146e9a82b
Christensen-Colter <51727656+Christensen-Colter@users.noreply.github.com>
2021-04-21 15:24:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
+// List of links for each week with a key value pair.
 const links = [
   { label: "Week 1", url: "index.html"}
 ];
 
+// Function called when page loads to display a dynamic list of links.
 function dynamicLinks() {
+
+  // A for loop counting through each item in our array.
   for (i = 0; i < links.length; i++) {
-        
+    
+    // Creating two different elements to work with.
     var li = document.createElement("li");
     var a = document.createElement("a");
 
+    // Creating a node for our elements to exist.
     var text = document.createTextNode(links[i].label);
-    
     a.appendChild(text);
 
+    // Adding details to our link element
     a.title = links[i].label;
     a.href = links[i].url;
     
+    // Add our link element to our list element
     li.appendChild(a);
-    document.getElementById("oList").appendChild(li);
-  
-  }
 
- 
+    // Add our list element to the ordered list element by Id.
+    document.getElementById("oList").appendChild(li);
+  }
 }
 
 
