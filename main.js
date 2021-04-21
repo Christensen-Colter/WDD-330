d85e2620f0bcabc6e367efc93c552968ae711304
Christensen-Colter <51727656+Christensen-Colter@users.noreply.github.com>
2021-04-21 12:06:07 -0600

    JS update for li

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const links = [
-  {
-    label: "Week1 notes",
-    url: "week1/index.html"
-  }
+    "Week1 notes",
+    "week1/index.html",
+    "Week2 notes",
+    "week2/index.html"
 ]
+
+function createLink(value) {
+  var x = document.createElement("li")
+  var t = document.createTextNode(value);
+  x.appendChild(t);
+  document.getElementById("demo").apendChild(x);
+}
+
+function dynamicLinks() {
+  for (i = 0; i < links.length; i++) {
+    //document.getElementById("demo").innerHTML = links[i];
+    document.createElement("li") = links[i];
+    //var t = document.createTextNode(links[i]);
+    //x.appendChild(t);
+    //document.getElementById("demo").apendChild(x);
+    //createLink(links[i]);
+  }
+
+ 
+}
+
 
