2daa4352d8a07daf90488fed92d3e8d10d24603e
Christensen-Colter <51727656+Christensen-Colter@users.noreply.github.com>
2021-05-06 08:57:58 -0600

    File Structure organization

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // List of links for each week with a key value pair.
 const links = [
   { label: "Week 1", url: "index.html"},
-  { label: "Week 2", url: "/Week 2/mathmachine.html"}
+  { label: "Week 2", url: "/Week 2/mathmachine.html"},
+  { label: "Week 3", url: ""}
 ];
 
 // Function called when page loads to display a dynamic list of links.
